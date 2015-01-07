c53d2ac5b3ec15961531ef9fe9c218c82e1c4cdd
Patrick O'Dwyer <paddy@turtledome.com>
2015-01-07 16:07:56 +0000

    Refactor player code into player entity

--- src/js/entities/player.js
+++ src/js/entities/player.js
@@ -1,6 +1,25 @@
+var cursors;
 var Player = function (game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'testsprite');
+    // Phaser.Sprite.call(this, game, x, y, 'testsprite');
+    // game.add.existing(this);
+    // The player and its settings
+    //this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+    Phaser.Sprite.call(this, game, x, y, 'dude');
     game.add.existing(this);
+ 
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(this);
+ 
+    //  Player physics properties. Give the little guy a slight bounce.
+    this.body.bounce.y = 0.2;
+    this.body.gravity.y = 300;
+    this.body.collideWorldBounds = true;
+ 
+    //  Our two animations, walking left and right.
+    this.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -10,6 +29,36 @@ Player.prototype.constructor = Player;
  * Automatically called by World.update
  */
 Player.prototype.update = function() {
+    //  Reset the players velocity (movement)    
+    this.body.velocity.x = 0;
+ 
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        this.body.velocity.x = -150;
+ 
+        this.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        this.body.velocity.x = 150;
+ 
+        this.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        this.animations.stop();
+ 
+        this.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && this.body.touching.down)
+    {
+        this.body.velocity.y = -350;
+    }
 };
 
 module.exports = Player;
--- src/js/states/game.js
+++ src/js/states/game.js
@@ -48,21 +48,7 @@ Game.prototype = {
     ledge.body.immovable = true;
 
     // The player and its settings
-    this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
- 
-    //  We need to enable physics on the player
-    this.game.physics.arcade.enable(this.player);
- 
-    //  Player physics properties. Give the little guy a slight bounce.
-    this.player.body.bounce.y = 0.2;
-    this.player.body.gravity.y = 300;
-    this.player.body.collideWorldBounds = true;
- 
-    //  Our two animations, walking left and right.
-    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.player = new Player(this.game, 32, this.game.world.height - 150);
   },
 
   update: function () {
@@ -85,36 +71,6 @@ Game.prototype = {
     // this.testentity.scale.x = scale * 0.6;
     // this.testentity.scale.y = scale * 0.6;
 
-    //  Reset the players velocity (movement)    
-    this.player.body.velocity.x = 0;
- 
-    if (this.cursors.left.isDown)
-    {
-        //  Move to the left
-        this.player.body.velocity.x = -150;
- 
-        this.player.animations.play('left');
-    }
-    else if (this.cursors.right.isDown)
-    {
-        //  Move to the right
-        this.player.body.velocity.x = 150;
- 
-        this.player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        this.player.animations.stop();
- 
-        this.player.frame = 4;
-    }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (this.cursors.up.isDown && this.player.body.touching.down)
-    {
-        this.player.body.velocity.y = -350;
-    }
   },
 
   onInputDown: function () {
